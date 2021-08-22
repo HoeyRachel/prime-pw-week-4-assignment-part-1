986abd2a5ff30b58500cbf976c7d0e0e98241d15
Rachel Hoey <rhoey@hotmail.com>
2021-08-21 20:11:36 -0500

    week 4 progress save 1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,36 +14,43 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(name) {
+  return 'Hello, Friend!'
 }
 // Remember to call the function to test
-
+console.log ('Should say Hello, Friend!', helloName());
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
+  let answer = firstNumber + secondNumber;
+  return answer;
   // return firstNumber + secondNumber;
 }
-
+console.log ('adding two numbers with firstNumber & second number:', addNumbers ( 9, 1 ) );
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree(num0, num1, num2 ){
+  let answer = num0*num1*num2;
+  return answer;
 
 }
-
+console.log ('multiplying three numbers:', multiplyThree( 1,1,5) );
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return;true
+  } else {
+    ( number < 0);
+    return;false
   }
-    return;
+    
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
+console.log( 'isPositive - should say false', isPositive( 0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
