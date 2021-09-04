5acb91b3ed888573af18b373d59eaa67414a9397
Rachel Hoey <rhoey@hotmail.com>
2021-09-03 22:15:57 -0500

    stretch goal 1 completed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -100,11 +100,25 @@ console.log ('Do we have enough room in the pantry?', find());
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
-}
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+function isFirstLetter(letterChecked) {
+console.log ('in firstLetter'); 
+ if(letterChecked[0]==='a'){
+  return true;
+}//end if 
+ else {
+  return false;
+}//end else
+
+}//end is firstLetter
+
+
+
+
+// console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+// console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+
+
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
